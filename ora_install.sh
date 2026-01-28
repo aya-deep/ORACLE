9a7f0c3911934355d22df2d302c0c6b6c01d3c2c
ayoub <ayoub.allali@deep.ey>
2026-01-28 23:01:44 +0100

    correction des indentations

--- ora_install.sh
+++ ora_install.sh
@@ -117,35 +117,40 @@ directory_creation() {
 }
 
 # Function for updating the kernel options for oracle database
-kernel_settings () {
-	echo "fs.file-max = 6815744
-	kernel.sem = 250 32000 100 128
-	kernel.shmmni = 4096
-	kernel.shmall = 1073741824
-	kernel.shmmax = 4398046511104
-	kernel.panic_on_oops = 1
-	net.core.rmem_default = 262144
-	net.core.rmem_max = 4194304
-	net.core.wmem_default = 262144
-	net.core.wmem_max = 1048576
-	net.ipv4.conf.all.rp_filter = 2
-	net.ipv4.conf.default.rp_filter = 2
-	fs.aio-max-nr = 1048576
-	net.ipv4.ip_local_port_range = 9000 65500" > /etc/sysctl.d/200-oracle.conf
-	
-	sysctl --system
+kernel_settings() {
+    cat <<EOF > /etc/sysctl.d/200-oracle.conf
+fs.file-max = 6815744
+kernel.sem = 250 32000 100 128
+kernel.shmmni = 4096
+kernel.shmall = 1073741824
+kernel.shmmax = 4398046511104
+kernel.panic_on_oops = 1
+net.core.rmem_default = 262144
+net.core.rmem_max = 4194304
+net.core.wmem_default = 262144
+net.core.wmem_max = 1048576
+net.ipv4.conf.all.rp_filter = 2
+net.ipv4.conf.default.rp_filter = 2
+fs.aio-max-nr = 1048576
+net.ipv4.ip_local_port_range = 9000 65500
+EOF
+
+    sysctl --system
 }
 
 # Function for editing the limits of the user oracle on the os for oracle database
 limits_setting () {
-	echo "oracle   soft   nofile    1024
-	oracle   hard   nofile    65536
-	oracle   soft   nproc    16384
-	oracle   hard   nproc    16384
-	oracle   soft   stack    10240
-	oracle   hard   stack    32768
-	oracle   hard   memlock    134217728
-	oracle   soft   memlock    134217728" > /etc/security/limits.d/oracle-database-preinstall-19c.conf
+	cat << EOF > /etc/security/limits.d/oracle-database-preinstall-19c.conf 
+oracle   soft   nofile    1024
+oracle   hard   nofile    65536
+oracle   soft   nproc    16384
+oracle   hard   nproc    16384
+oracle   soft   stack    10240
+oracle   hard   stack    32768
+oracle   hard   memlock    134217728
+oracle   soft   memlock    134217728
+EOF
+
 }
 
 # Function to export the env var for oracle database
