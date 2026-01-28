8f98a1f99643808d704dc2d4e472a4e417f0bdb5
ayoub <ayoub.allali@deep.ey>
2026-01-28 22:34:07 +0100

    ajout d'une première version

---
+++ ora_install.sh
@@ -0,0 +1,226 @@
+#!/bin/bash
+
+###############################################################################
+# Script Name   : install_oracle.sh
+# Description   : Installation of Oracle database 19
+# Author        : Allali Ayoub
+# Created       : 2026-01-28
+# Version       : 1.0
+# Usage         : sudo ./install_oracle.sh
+###############################################################################
+
+# Check if the user is root
+if [[ $UID -ne 0 ]]; then
+	echo "To install oracle on your machine, you need to be root !"
+	exit 1
+fi
+
+# Function fot the creation of the necessary groups
+func_groups () {
+groups=(oinstall dba oper backupdba dgdba kmdba asmdba asmoper asmadmin racdba)
+gid_value=54321
+
+for group in "${groups[@]}"; do
+	groupadd -g "$gid_value" "$group" 2>/dev/null
+	if [[ $? -eq 0 ]]; then
+		echo "$group group created"
+	else
+		if getent group "$group" > /dev/null; then
+			echo "$group group already exists"
+		else
+			echo "Failed to create $group"
+		fi
+	fi
+	((gid_value++))
+done
+
+}
+
+# Function fot the creation of the necessary users
+func_users() {
+	useradd -u 54321 -m -g oinstall -G dba,oper oracle 2>/dev/null
+	if [[ $? -eq 0 ]]; then
+		echo "oracle user created"
+	else
+		if getent passwd oracle > /dev/null; then
+			echo "oracle user already exists"
+		else
+			echo "Failed to create user oracle"
+		fi
+	fi
+
+	echo "oracle:oracle" | chpasswd
+}
+
+# Function to install the necessary packages for oracle database
+install_packages () {
+
+	packages=(
+	bc
+	binutils
+	compat-openssl11
+	elfutils-libelf
+	fontconfig
+	glibc
+	glibc-devel
+	ksh
+	libaio
+	libasan
+	liblsan
+	libX11
+	libXau
+	libXi
+	libXrender
+	libXtst
+	libxcrypt-compat
+	libgcc
+	libibverbs
+	libnsl
+	librdmacm
+	libstdc++
+	libxcb
+	libvirt-libs
+	make
+	policycoreutils
+	policycoreutils-python-utils
+	smartmontools
+	sysstat
+	glibc-headers
+	ipmiutil
+	libnsl2
+	libnsl2-devel
+	net-tools
+	nfs-utils
+	gcc 
+	unixODBC
+	)
+
+	for package in ${packages[@]}; do
+		dnf install -y $package > /dev/null
+		if [[ $? -eq 0 ]]; then
+			echo "Package $package installed"
+		fi
+	done
+
+}
+
+# Function for the creation of the necessary directory for oracle database
+directory_creation() {
+	mkdir -p /u01/app
+        mkdir -p /u01/app/oraInventory
+	mkdir -p /u02/app/oracle/oradata
+	mkdir -p /u01/app/oracle/fast_recovery_area
+	mkdir -p /u01/app/oracle/product/19.0.0/dbhome_1
+
+	chown -R oracle:oinstall /u01 /u02
+	chmod -R 755 /u01 /u02
+}
+
+# Function for updating the kernel options for oracle database
+kernel_settings () {
+	echo "fs.file-max = 6815744
+	kernel.sem = 250 32000 100 128
+	kernel.shmmni = 4096
+	kernel.shmall = 1073741824
+	kernel.shmmax = 4398046511104
+	kernel.panic_on_oops = 1
+	net.core.rmem_default = 262144
+	net.core.rmem_max = 4194304
+	net.core.wmem_default = 262144
+	net.core.wmem_max = 1048576
+	net.ipv4.conf.all.rp_filter = 2
+	net.ipv4.conf.default.rp_filter = 2
+	fs.aio-max-nr = 1048576
+	net.ipv4.ip_local_port_range = 9000 65500" > /etc/sysctl.d/200-oracle.conf
+	
+	sysctl --system
+}
+
+# Function for editing the limits of the user oracle on the os for oracle database
+limits_setting () {
+	echo "oracle   soft   nofile    1024
+	oracle   hard   nofile    65536
+	oracle   soft   nproc    16384
+	oracle   hard   nproc    16384
+	oracle   soft   stack    10240
+	oracle   hard   stack    32768
+	oracle   hard   memlock    134217728
+	oracle   soft   memlock    134217728" > /etc/security/limits.d/oracle-database-preinstall-19c.conf
+}
+
+# Function to export the env var for oracle database
+env_var () {
+	echo "export ORACLE_BASE=/u01/app/oracle     
+	export ORACLE_HOME=$ORACLE_BASE/product/19.0.0/dbhome_1 
+	export ORACLE_SID=ORCL                 
+	export PATH=$ORACLE_HOME/bin:$PATH      
+	export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
+	" > /home/oracle/.bashrc
+}
+
+# Function for editing the SELINUX for oracle database
+se_linux () {
+
+	setenforce 0
+}
+
+# Function to recup the zip file for the installation of oracle database
+install_zip () {
+	zip_path="$1"
+	if [ ! -f $zip_path ]; then
+		echo "error: zip file doesn't exist"
+		return 1
+	fi
+	unzip "$zip_path" -d /u01/app/oracle/product/19.0.0/dbhome_1
+}
+
+# Main function to execute everything
+main() {
+    echo "========================================="
+    echo "Installation Oracle Database 19c"
+    echo "========================================="
+    echo ""
+    
+    echo "=== Création des groupes ==="
+    func_groups
+    echo ""
+    
+    echo "=== Création de l'utilisateur oracle ==="
+    func_users
+    echo ""
+    
+    echo "=== Installation des packages ==="
+    install_packages
+    echo ""
+    
+    echo "=== Création des répertoires ==="
+    directory_creation
+    echo ""
+    
+    echo "=== Configuration kernel ==="
+    kernel_settings
+    echo ""
+    
+    echo "=== Configuration limites système ==="
+    limits_setting
+    echo ""
+    
+    echo "=== Configuration variables d'environnement ==="
+    env_var
+    echo ""
+    
+    echo "=== Désactivation SELinux ==="
+    se_linux
+    echo ""
+    
+    echo "========================================="
+    echo "✅ Préparation système terminée !"
+    echo "========================================="
+    echo ""
+    echo "Pour installer Oracle :"
+    echo "1. Lance : sudo ./install_oracle.sh /chemin/vers/oracle.zip"
+    echo "2. Puis : su - oracle"
+    echo "3. Puis : cd \$ORACLE_HOME && ./runInstaller"
+}
+
+main
