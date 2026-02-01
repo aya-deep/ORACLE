ed425e6a5a9417515aee56838a20c523fa190982
ayoub <ayoub.allali@digora.com>
2026-02-01 13:51:26 +0100

    correction coquille

--- ora_install.sh
+++ ora_install.sh
@@ -175,12 +175,12 @@ EOF
 
 # Function to export the env var for oracle database
 env_var () {
-	cat << EOF > /home/oracle/.bashrc
+	cat << EOF >> /home/oracle/.bashrc
 export ORACLE_BASE=/u01/app/oracle     
-export ORACLE_HOME=$ORACLE_BASE/product/19.0.0/dbhome_1 
+export ORACLE_HOME="$ORACLE_BASE"/product/19.0.0/dbhome_1 
 export ORACLE_SID=ORCL                 
-export PATH=$ORACLE_HOME/bin:$PATH      
-export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
+export PATH="$ORACLE_HOME"/bin:"$PATH"      
+export LD_LIBRARY_PATH="$ORACLE_HOME"/lib:"$LD_LIBRARY_PATH"
 EOF
 }
 
